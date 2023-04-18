f2fc9e8a89ef8fe283723a835bfddba7f386865f
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-04-18 18:25:54 -0300

    Refatorações feitas

--- mobile/telemed_app/lib/main.dart
+++ mobile/telemed_app/lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:telemed_app/routes/routes.dart';
+import 'package:telemed_app/login/view/login_view.dart';
+import 'package:telemed_app/signup/view/singup_view.dart';
+import 'package:telemed_app/utils/routes/routes.dart';
 import 'package:telemed_app/theme_provider.dart';
-import 'package:telemed_app/themes/app_themes.dart';
+import 'package:telemed_app/utils/themes/app_themes.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -16,10 +24,16 @@ class MyApp extends StatelessWidget {
       child: Consumer<ThemeProvider>(
         builder: (context, theme, _) {
           return MaterialApp(
-            title: 'Flutter Demo',
+            builder: Asuka.builder,
+            navigatorObservers: [Asuka.asukaHeroController],
+            title: 'TELEMED-APP',
             theme:
                 theme.isDark ? AppThemes().darkTheme : AppThemes().whiteTheme,
-            routes: appRoutes,
+            routes: {
+              AppRoutes.HOME: (BuildContext context) => const Placeholder(),
+              AppRoutes.LOGIN: (BuildContext context) => LoginView(),
+              AppRoutes.SIGNON: (BuildContext context) => const SignUpView(),
+            },
             initialRoute: '/',
           );
         },
---
+++ mobile/telemed_app/lib/utils/themes/app_themes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:telemed_app/utils/animations/transition_animation.dart';
+
+class AppThemes {
+  ThemeData darkTheme = ThemeData(
+    pageTransitionsTheme: PageTransitionsTheme(
+        builders: {TargetPlatform.android: CustomPageTransitionBuilder()}),
+    colorScheme: const ColorScheme.dark(
+      background: Color.fromARGB(0, 7, 23, 6),
+      primary: Color.fromRGBO(244, 244, 244, 1),
+    ),
+    fontFamily: 'Montserrat',
+  );
+  ThemeData whiteTheme = ThemeData(
+    pageTransitionsTheme: PageTransitionsTheme(
+        builders: {TargetPlatform.android: CustomPageTransitionBuilder()}),
+    colorScheme: const ColorScheme.light(
+      background: Color.fromRGBO(244, 244, 244, 1),
+      primary: Colors.black,
+    ),
+    fontFamily: 'Montserrat',
+  );
+}
