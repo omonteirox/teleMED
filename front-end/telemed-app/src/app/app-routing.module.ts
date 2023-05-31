377c9b9e022688ca3ec656c492c09d9256f8d282
Maria Eduarda Gonçalves <mariiagoncalves.if@gmail.com>
2023-05-31 15:02:27 -0300

    Icon

--- front-end/telemed-app/src/app/app-routing.module.ts
+++ front-end/telemed-app/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { DashboardInterpreteComponent } from './components/dashboard-interprete/dashboard-interprete.component';
+import { DashboardPacienteComponent } from './components/dashboard-paciente/dashboard-paciente.component';
+import { DashboardMedicoComponent } from './components/dashboard-medico/dashboard-medico.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'DashboardInterprete', component: DashboardInterpreteComponent },
+  { path: 'DashboardPaciente', component: DashboardPacienteComponent },
+  { path: 'DashboardMedico', component: DashboardMedicoComponent },
 ];
 
 @NgModule({
